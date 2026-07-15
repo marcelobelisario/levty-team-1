8a9912aa097d2e9914aa5ae1743fd7faff008ed7
Marcelo Belisário <belisario.profissional@outlook.com>
2026-07-14 23:50:22 -0300

    feat: adiciona crud completo da entidade pessoa

--- backend/routes.js
+++ backend/routes.js
@@ -2,10 +2,12 @@ const { Router } = require("express")
 
 const estacionamentoRoutes = require("./src/modules/estacionamento/routes/estacionamento.routes")
 const pisoRoutes = require("./src/modules/piso/routes/piso.routes")
+const pessoaRoutes = require("./src/modules/pessoa/routes/pessoa.routes")
 
 const rotas = Router()
 
 rotas.use('/estacionamentos', estacionamentoRoutes)
 rotas.use('/pisos', pisoRoutes)
+rotas.use('/pessoa', pessoaRoutes)
 
 module.exports = rotas
